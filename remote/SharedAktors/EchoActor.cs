1f80f5c8c095dbe89a1a6ada7c5a512e36fdbc8e
ENuzhdin <Evgeny.Nuzhdin@digitalplant.ru>
2018-12-05 12:58:09 +0300

    remote deploy doc example

---
+++ remote/SharedAktors/EchoActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Akka.Actor;
+
+namespace SharedAktors
+{
+    public class EchoActor : ReceiveActor
+    {
+        public EchoActor()
+        {
+            Receive<Hello>(hello =>
+            {
+                Console.WriteLine("[{0}]: {1}", Sender, hello.Message);
+                Sender.Tell(hello);
+            });
+        }
+    }
+}
